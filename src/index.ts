71722d557ae73478feacf1a369a1406330d67ab1
zizifn <1803942+zizifn@users.noreply.github.com>
2024-09-18 11:09:34 +0800

    update code

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,31 @@ async function main() {
     did: MI_DID, // Device ID or Name (optional - fill in after retrieving from the device list)
   };
   const MiNA = await getMiNA(config);
+  await MiNA?.play(
+    {
+        tts: '你好，我是小爱同学，有什么可以帮助你的吗？',
+    }
+  )
   const MiIOT = await getMiIOT(config);
 //   console.log("MiNA devices", await MiNA?.getDevices());
 
-  setInterval(async () => {
-    const msgs = await MiNA?.getConversations({
-        limit: 1
-      })
-      console.dir(msgs?.records ,  {depth: 10});
-      const requestId = msgs?.records[0]?.requestId|| ''
-      if(!msg.has(requestId)){
-        msg.set(msgs?.records[0]?.requestId|| '', msgs?.records[0]);
-        const tts = msgs?.records[0]?.answers.find((a: any) => a.type === 'TTS') || '';
-        console.log('---------', tts);
-        await MiIOT?.doAction(5, 1, `回答已经被截断，我的上一个问题是，${msgs?.records[0]?.query}. 答案是：${tts.tts.text}`);
-      }
-  }, 1000);
+//   setInterval(async () => {
+//     const msgs = await MiNA?.getConversations({
+//         limit: 1
+//       })
+//       console.dir(msgs?.records ,  {depth: 10});
+//       const requestId = msgs?.records[0]?.requestId|| ''
+//       if(!msg.has(requestId)){
+//         msg.set(msgs?.records[0]?.requestId|| '', msgs?.records[0]);
+//         const tts = msgs?.records[0]?.answers.find((a: any) => a.type === 'TTS') || '';
+//         console.log('---------', tts);
+//         await MiIOT?.doAction(5, 1, `回答已经被截断，我的上一个问题是，${msgs?.records[0]?.query}. 答案是：${tts.tts.text}`);
+//       }
+//   }, 1000);
 
 
 //   console.log("MiIOT devices", await MiIOT?.getDevices());
+//   await MiIOT?.doAction(2,2, 'true');
   // Find your device Spec here: https://home.miot-spec.com/
   
 }
